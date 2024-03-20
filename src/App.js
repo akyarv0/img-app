d40d373aed435a53a1f97b4f480d2bf362713ad8
akyarv0 <akyarv0@gmail.com>
2024-03-20 18:02:46 +0300

    map

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ function App() {
   // const arr = data.map(item => item.src.large);
   // console.log(arr);
   return (
-
     <div>
       <div className="title">Image Gallery</div>
       <div className="container">
-        {
-          data.map((item,index)=>
-            <div key={index} className="item">
-            <img src={item.src.large} alt=""/>
+        {data.map((item, index) => (
+          <div key={index} className="item">
+            <img src={item.src.large} alt="" />
             <p>{item.photographer}</p>
-            </div>
-            
-          )
-        }
+          </div>
+        ))}
       </div>
     </div>
   );
